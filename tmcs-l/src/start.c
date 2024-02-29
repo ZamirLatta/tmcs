c9c476ae4da99ab2d63fd7fdc605c8b6254ef0ff
ZamirLatta <114021616+ZamirLatta@users.noreply.github.com>
2024-02-29 08:02:32 -0500

    other version for linux

---
+++ tmcs-l/src/start.c
@@ -0,0 +1,13 @@
+// start program
+//#include "/home/zmr_01001100/tmcs_/tmcs_/tmcs-l/lib/start.h"
+#include <stdio.h>
+
+
+int start(int startVal)
+{
+    printf("Press 1 to Start Game:\n");
+    startVal = scanf("%d", &startVal);
+    int start = startVal;
+    printf("test");
+    return startVal;
+}
